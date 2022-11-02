9a3f015ad4366be02b45ce22254304662a023426
Francis Peck <frncspeck@gmail.com>
2022-11-02 14:29:17 +0000

    coding coffee episode 3

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,22 @@
 
 # A very simple Flask Hello World app for you to get started with...
+import os
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired
 
-from flask import Flask
+class MyForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(12).hex()
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return "Hello from Between2Cracks!"
+    form = MyForm()
+    if request.method == 'POST':
+        print(form.name.data, form.password.data)
+    return render_template('form.html', form=form)
 
